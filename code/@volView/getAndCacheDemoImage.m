60ade870adfd3a81fd38cbbd0a3a09dd92d3c129
Rob Campbell <git@raacampbell.com>
2019-10-10 15:16:57 +0100

    New, nicer, demo image that is square

--- code/@volView/getAndCacheDemoImage.m
+++ code/@volView/getAndCacheDemoImage.m
@@ -4,6 +4,6 @@ function getAndCacheDemoImage(obj)
 
     if ~exist(obj.cachedDemoDataLocation,'file')
         fprintf('Downloading demo image stack and caching to disk for future use.\n')
-        websave(obj.cachedDemoDataLocation,'http://mouse.vision/lasagna/reference.tiff');
-    end            
+        websave(obj.cachedDemoDataLocation,'http://mouse.vision/lasagna/template.tif');
+    end
 end
