96e2138a2af4792a66b0f3e856493483d172c937
Rob Campbell <git@raacampbell.com>
2019-10-12 16:43:53 +0100

    Complete example function for finding borders in a 3D object.

--- examples/generateBorders.m
+++ examples/generateBorders.m
@@ -1,61 +1,88 @@
 function B = generateBorders(im,doPlot)
     % Demo showing how to get borders from demo image in order to overlay them volView
     % 
-    % How to use
-    % V=volView; % To get the demo image of a mouse brain
-    % im=V.imStackOrig; % Get the image stack out of the object
-    % B = generateBorders(im);
+    % function B = generateBorders(im,doPlot)
     %
-    % A plot of the overlaid borders appears and the borders are returned 
-    % in a cell array. B{1} is produced whilst looping over dim 3 of the
-    % image, B{2} whilst looping over dim 2, and B{3} whilst looping over 
-    % dim 1. 
+    % Purpose
+    % This example function demonstrates how to trace borders around an object in an image stack
+    % in order to allow overlay of these borders in volView.  A plot of the overlaid borders
+    % optionally appears and the borders are returned as a cell array of cell arrays.
+    %x
+    %
+    % Inputs
+    % im - The image stack containing the object around which borders are to be drawn.
+    % doPlot - False by default. If true, a new figure window appears displaying the
+    %          three orthogonal views with the borders drawn around the object 
+    % threshLevel - The value at which to threshold the image to pick out the object.
+    %               By default this is suitable for the demo mouse brain image stack.
+    %
+    % Outputs
+    % B - A cell array of borders suitable for plotting in volView
+    %
+    %
+    % Example usage:
+    % V=volView; % To get the demo image of a mouse brain
+    % im = V.imStackOrig;
+    % B = generateBorders(im);
+    % delete(V)
+    % V=volView(im,[],B)
+    %
+    %
     %
     % Rob Campbell - Sainsbury Wellcome Centre, 2019
 
 
-
+    % Parse input arguments
     if nargin==0
         help(mfilname)
     end
 
-    if nargin<2
-        doPlot=true;
+    if nargin<2 || isempty(doPlot)
+        doPlot=false;
     end
 
-    
+    if nargin<3 || isempty(threshLevel)
+        threshLevel=10;
+    end
+
+
+    % Loop through the three axes, calculating borders
     for ii=1:3
-        B{ii}=getBordersAlongDim(im,ii-1);
+        B{ii}=getBordersAlongDim(im,ii-1,threshLevel);
     end
 
+
+    % Bale out if the user didn't ask for the debug plot to be shown
     if ~doPlot
         return
     end
 
 
 
-    % Following code demonstrates how to overlay the borders
+    % Overlay borders onto object
     figure
-    ii=200;
-    
+
     subplot(1,3,1)
-    imagesc(im(:,:,ii))
+    tPlane = round(size(im,3)/2); % Middle plane of stack along this axis
+    imagesc(im(:,:,tPlane))
     hold on
-    t=B{1}{ii}{1};
+    t=B{1}{tPlane}{1};
     plot(t(:,2),t(:,1),'-r')
     axis equal tight
 
     subplot(1,3,2)
-    imagesc(squeeze(im(:,ii,:)))
+    tPlane = round(size(im,2)/2);
+    imagesc(squeeze(im(:,tPlane,:)))
     hold on
-    t=B{2}{ii}{1};
+    t=B{2}{tPlane}{1};
     plot(t(:,1),t(:,2),'-r')
     axis equal tight
 
     subplot(1,3,3)
-    imagesc(squeeze(im(ii,:,:)))
+    tPlane = round(size(im,1)/2);
+    imagesc(squeeze(im(tPlane,:,:)))
     hold on
-    t=B{3}{ii}{1};
+    t=B{3}{tPlane}{1};
     plot(t(:,2),t(:,1),'-r')
     axis equal tight
 
@@ -63,16 +90,20 @@ function B = generateBorders(im,doPlot)
 
 
 
-    function [B,bw] = getBordersAlongDim(im,permBy)
+    function [B,bw] = getBordersAlongDim(im,permBy,threshLevel)
+        % Workhorse function that calculates the borders for each
+        % slice along a given dimension
         im = permute(im, circshift([1,2,3],permBy));
         bw=zeros(size(im));
         SE = strel('disk',5);
         B={};
 
         for ii=1:size(im,3)
-            bw(:,:,ii) = im(:,:,ii)>10;
+            bw(:,:,ii) = im(:,:,ii)>threshLevel;
             bw(:,:,ii)=imdilate(bw(:,:,ii),SE); 
             bw(:,:,ii)=imerode(bw(:,:,ii),SE);
+
+            %Sort boundaries by length so largest boundary is always first
             tmp=bwboundaries(bw(:,:,ii),'noholes');
             [~,ind]=sort(cellfun(@length,tmp),'descend');
             B{ii} = tmp(ind);
